948d16213eeff741db9ee223f09a065272666b1c
Ikenna <ikenna@kali.Ikenna-PC>
2022-08-24 08:52:08 -0400

    make some corrections in task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,5 @@
 for n in range(0, 100):
     if n < 10:
         print("0{},".format(n), end=" ")
-    elif n < 99:
-        print("{},".format(n), end=" ")
-    else:
-        print("{}".format(n), end=" ")
+    elif n <= 99:
+        print("{},".format(n) if n < 99 else "{}".format(n), end=" ")
