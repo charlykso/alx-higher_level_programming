5437d46119c424c612179ac42b981ef07aa3d11d
Ikenna <ikenna@kali.Ikenna-PC>
2022-08-24 11:12:51 -0400

    did tast 2, 3, 4 and 5

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+for alpha_letters in range(97, 122):
+      print("{:c}".format(alpha_letters), end="")
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for n in range(0, 100):
+    if n < 10:
+        print("0{},".format(n), end=" ")
+    else:
+        print("{},".format(n), end=" ")
