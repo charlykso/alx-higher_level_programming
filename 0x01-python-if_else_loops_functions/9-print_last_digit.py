563aac6dffac65c45f5926a6cb859fd352c6a781
Ikenna <ikenna@kali.Ikenna-PC>
2022-08-24 20:43:44 -0400

    corrected task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,6 +4,8 @@ def print_last_digit(number):
     x = abs(number)
     if x >= 10:
         last = x % 10
+        print("{}".format(last))
     else:
         last = x
+        print("{}".format(last))
     return last
