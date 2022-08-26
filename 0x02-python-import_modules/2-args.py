a1febd51cdd4336c99cdc373d55e95e63397c7d5
Ikenna <ikenna@kali.Ikenna-PC>
2022-08-25 23:05:38 -0400

    command line argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-
 import sys
-if len(sys.argv) == 1:
-    print(".")
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    j = 1
-    while j < len(sys.argv):
-        print("{}: {}".format(j, sys.argv[j]))
-        j = j + 1
+
+
+def main():
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        j = 1
+        while j < len(sys.argv):
+            print("{}: {}".format(j, sys.argv[j]))
+            j = j + 1
+
+
+if __name__ == "__main__":
+    main()
