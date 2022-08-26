dd9225f7058fc9ff27b6f4cb96fa44f20ee47e4b
Ikenna <ikenna@kali.Ikenna-PC>
2022-08-26 00:43:44 -0400

    Fast alphabet

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, mul, sub, div
+else:
+    a = int(argv[1])
+    operator = str(argv[2])
+    b = int(argv[3])
+    if operator == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif operator == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif operator == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif operator == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
+        print("{}".format(
+            'Unknown operator.Available operators: +, -, * and /'))
+        exit(1)
