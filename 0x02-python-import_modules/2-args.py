f3ae1330a100ac200476bce4d54a2b12f3bb3d19
Ikenna <ikenna@kali.Ikenna-PC>
2022-08-25 22:59:13 -0400

    command lind argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 import sys
-print("{} arguments:".format(len(sys.argv) - 1))
-j = 1
-while j < len(sys.argv):
-    print("{}: {}".format(j, sys.argv[j]))
-    j = j + 1
+if len(sys.argv) == 1:
+    print(".")
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    j = 1
+    while j < len(sys.argv):
+        print("{}: {}".format(j, sys.argv[j]))
+        j = j + 1
