51acc6ee1d1f92569c46fcefdf95be1ac9aa58ba
Ikenna <ikenna@kali.Ikenna-PC>
2022-09-12 23:46:30 -0400

    Safe function

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-	counter = 0
-	for i in range(x):
-		try:
-			print(my_list[i], end='')
-			counter += 1
-		except Exception as error:
-			break
-	print('')
-	return counter
+    counter = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+            counter += 1
+        except Exception as error:
+            break
+    print('')
+    return counter
