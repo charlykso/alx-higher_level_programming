7f91808c8885385ad577e8158918786c8824ab55
Ikenna <ikenna@kali.Ikenna-PC>
2022-09-13 21:07:48 -0400

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        try:
+            self.__size = size
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
