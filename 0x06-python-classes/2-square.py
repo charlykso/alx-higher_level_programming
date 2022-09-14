c6b2ea4dada6968b06e09aaff126867a149f0313
Ikenna <ikenna@kali.Ikenna-PC>
2022-09-13 21:28:57 -0400

    Access and update private attribute

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,9 +2,9 @@
 
 class Square:
     def __init__(self, size=0):
-        try:
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
             self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,12 +2,12 @@
 
 class Square:
     def __init__(self, size=0):
-        try:
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
             self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
 
     def area(self):
-        return self.__size * self.__size
+        return (self.__size)**2
