7584164ea11e8ef840fc5d07e44a624309170183
Ikenna <ikenna@kali.Ikenna-PC>
2022-09-26 21:58:23 -0400

    Rectangle

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A class that Contains an area method that is not implemented
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def area(self):
+        """area method"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """integer validator method"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
