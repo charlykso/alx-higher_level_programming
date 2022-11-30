b19ec76d33eeceb5bfcc6795b4f2706e129b1a71
Ikenna <ikenna@kali.Ikenna-PC>
2022-11-30 09:04:54 -0500

    square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const BaseSquare = require('./5-square');
+
+module.exports = class Square extends BaseSquare {
+	charPrint(c){
+		if (c === undefined) {
+			this.print();
+		} else {
+			for (let j = 0; j < this.width; j++) {
+				console.log(c.repeat(this.height));
+			}
+		}
+	}
+};
