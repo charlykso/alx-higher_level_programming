cc3b5005d9bf2dec530731f2f7f7cf117ab167e5
Ikenna <ikenna@kali.Ikenna-PC>
2022-12-05 11:07:15 -0500

    City relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 """
-script that prints the State object with
-the name passed as argument from the database hbtn_0e_6_usa
+script that creates the State “California”
+with the City “San Francisco” from the database
+hbtn_0e_100_usa
 """
 
 from sys import argv
-from model_state import Base, State
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -24,8 +26,13 @@ if __name__ == '__main__':
     # create an instance of the Session
     session = Session()
 
-    # query the database to add new state
-    new_state = State(name='Louisiana')
+    # query the database to add new state and city
+    new_state = State(name='California')
+    new_city = City(name="San Francisco")
+
+    new_state.cities.append(new_city)
+
     session.add(new_state)
+    session.add(new_city)
+
     session.commit()
-    print('{}'.format(new_state.id))
