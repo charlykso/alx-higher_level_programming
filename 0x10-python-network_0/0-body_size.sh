3007b724b520730a575e0b5d9a3c8ae4d8d0e248
Ikenna <ikenna@kali.Ikenna-PC>
2022-12-15 20:51:19 -0500

    (fixed 1.0)cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, 
-# and displays the size of the body of the response.
-# usage: ./0-body_size.sh 0.0.0.0:5000
-
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
 curl -sI "$1" | grep Content-Length: | cut -d' ' -f2
