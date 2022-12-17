f17ab1610e1a1bc109910dea3362a10a468844f3
Ikenna <ikenna@kali.Ikenna-PC>
2022-12-17 00:47:25 -0500

    (fixed)What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,8 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 import requests
 
 
-if __name__ == '__main__':
-    r = requests.get('https://alx-intranet.hbtn.io/status')
+if __name__ == "__main__":
+    url = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- types: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    print('\t- type: {}'.format(type(url.text)))
+    print('\t- content: {}'.format(url.text))
