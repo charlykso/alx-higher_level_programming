c6f7c851e3665c41aef0b9576bf5b6139d5cb311
Ikenna <ikenna@kali.Ikenna-PC>
2022-12-16 22:28:34 -0500

    (fixed)Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,20 +7,24 @@ def find_peak(list_of_integers):
     '''
     Finds the peak integer
     '''
-    my_list = list_of_integers
-    x = len(my_list)
-    count = 0
-    y = count + 1
-    peak = 0
-    if x != 0:
-        while count < x:
-            while y + 1 < x:
-                if my_list[count] > my_list[y]:
-                    peak = my_list[count]
-                else:
-                    peak = my_list[y]
-                y = y + 1
-            count = count + 1
-    else:
+    if len(list_of_integers) == 0:
         return None
-    return peak
+
+    lists = list_of_integers
+    beg = 0
+    end = len(lists)-1
+
+    if lists[beg] > lists[beg+1]:
+        return lists[beg]
+    if lists[end] > lists[end-1]:
+        return lists[end]
+
+    mid = (beg+end)//2
+    if lists[mid-1] < lists[mid] and lists[mid+1] < lists[mid]:
+        return lists[mid]
+    if lists[mid] < lists[mid-1]:
+        return find_peak(lists[beg:mid+1])
+    elif lists[mid] < lists[mid+1]:
+        return find_peak(lists[mid:end+1])
+    else:
+        return lists[beg]
