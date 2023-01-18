68376ca665aba8dc33c2638c00c448fa0ee4a657
Ikenna <ikenna@kali.Ikenna-PC>
2023-01-18 01:04:37 -0500

    How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+request.get(url, function (err, res, data) {
+  if (err) {
+    console.log(err);
+  } else if (res.statusCode === 200) {
+    const dic = {};
+    const tasks = JSON.parse(data);
+    for (const i in tasks) {
+      if (tasks[i].completed) {
+        if (dic[tasks[i].userId] === undefined) {
+          dic[tasks[i].userId] = 1;
+        } else {
+          dic[tasks[i].userId]++;
+        }
+      }
+    }
+    console.log(dic);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
